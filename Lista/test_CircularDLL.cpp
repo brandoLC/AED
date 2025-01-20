43519dfcf96e6909ee20200de53e463763472a1a
Brando Lopez <brando.lopez@utec.edu.pe>
2025-01-20 11:36:14 -0500

    corrección de test para el Circular Doubly Linked List

--- Lista/test_CircularDLL.cpp
+++ Lista/test_CircularDLL.cpp
@@ -1,45 +1,54 @@
-//
-// Created by brano on 17/01/2025.
-//
-
 #include "CircularDLL.h"
 #include <gtest/gtest.h>
 
-TEST(CircularDLLTest, PushFrontBackTest) {
-    List<int> list; 
+TEST(CircularDLLTest, PushFrontTest) {
+    List<int> list;
     list.push_front(1);
-    list.push_back(2);
     list.push_front(0);
-    list.push_back(3);
-
     EXPECT_EQ(list.front(), 0);
-    EXPECT_EQ(list.back(), 3);
-    EXPECT_EQ(list.size(), 4);
+    EXPECT_EQ(list.size(), 2);
 }
 
-TEST(CircularDLLTest, PopFrontBackTest) {
+TEST(CircularDLLTest, PushBackTest) {
     List<int> list;
     list.push_back(1);
     list.push_back(2);
-    list.push_back(3);
-    list.push_back(4);
-    list.push_back(5);
-
-    EXPECT_EQ(list.pop_front(), 1);
-    EXPECT_EQ(list.pop_back(), 5);
-    EXPECT_EQ(list.size(), 3);
-    EXPECT_EQ(list.front(), 2);
-    EXPECT_EQ(list.back(), 4);
+    EXPECT_EQ(list.back(), 2);
+    EXPECT_EQ(list.size(), 2);
 }
 
-TEST(CircularDLLTest, InsertRemoveTest) {
+TEST(CircularDLLTest, PopFrontTest) {
+    List<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    EXPECT_EQ(list.pop_front(), 1);
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.front(), 2);
+}
+
+TEST(CircularDLLTest, PopBackTest) {
+    List<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    EXPECT_EQ(list.pop_back(), 2);
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.back(), 1);
+}
+
+TEST(CircularDLLTest, InsertTest) {
     List<int> list;
     list.push_back(1);
     list.push_back(3);
     list.insertAt(2, 1);
     EXPECT_EQ(list[1], 2);
     EXPECT_EQ(list.size(), 3);
+}
 
+TEST(CircularDLLTest, RemoveTest) {
+    List<int> list;
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
     list.remove(1);
     EXPECT_EQ(list[1], 3);
     EXPECT_EQ(list.size(), 2);
@@ -50,26 +59,29 @@ TEST(CircularDLLTest, OperatorTest) {
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
-    list.push_back(4);
-    list.push_back(5);
-
     EXPECT_EQ(list[0], 1);
     EXPECT_EQ(list[1], 2);
     EXPECT_EQ(list[2], 3);
-    EXPECT_EQ(list[3], 4);
-    EXPECT_EQ(list[4], 5);
 }
 
-TEST(CircularDLLTest, EmptySizeClearTest) {
+TEST(CircularDLLTest, EmptyTest) {
     List<int> list;
     EXPECT_TRUE(list.empty());
-    EXPECT_EQ(list.size(), 0);
+    list.push_back(1);
+    EXPECT_FALSE(list.empty());
+}
 
+TEST(CircularDLLTest, SizeTest) {
+    List<int> list;
+    EXPECT_EQ(list.size(), 0);
+    list.push_back(1);
+    EXPECT_EQ(list.size(), 1);
+}
+
+TEST(CircularDLLTest, ClearTest) {
+    List<int> list;
     list.push_back(1);
     list.push_back(2);
-    EXPECT_FALSE(list.empty());
-    EXPECT_EQ(list.size(), 2);
-
     list.clear();
     EXPECT_TRUE(list.empty());
     EXPECT_EQ(list.size(), 0);
@@ -80,18 +92,9 @@ TEST(CircularDLLTest, ReverseTest) {
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
-    list.push_back(4);
-    list.push_back(5);
-
     list.reverse();
-
-    EXPECT_EQ(list.front(), 5);
+    EXPECT_EQ(list.front(), 3);
     EXPECT_EQ(list.back(), 1);
-
-    list.pop_front(); EXPECT_EQ(list.front(), 4);
-    list.pop_front(); EXPECT_EQ(list.front(), 3);
-    list.pop_front(); EXPECT_EQ(list.front(), 2);
-    list.pop_front(); EXPECT_EQ(list.front(), 1);
 }
 
 int main(int argc, char **argv) {
